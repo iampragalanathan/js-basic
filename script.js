fb1e2ed43c46d9a0c7fa5a2268389eb01b899844
PRAGALANATHAN A <pragalanathan.eng@gmail.com>
2025-07-30 19:24:27 +0530

    change

---
+++ script.js
@@ -0,0 +1,81 @@
+// ============Task 1===============
+
+// let person={name:"Pragalanathan",age:"22",country:"India"}
+// function logdata(){
+
+//     console.log(person.name, "is",person.age, "years old and lives in",person.country)
+// }
+// logdata()
+
+
+// ===========Task 2===============
+
+// let age=68
+
+// if(age <= 6){
+
+//     console.log("free")
+// }
+// else if( age <=17){
+
+//     console.log(" child discount")
+// }
+// else if (age <= 26){
+
+//     console.log("student discount")
+// }
+// else if (age <= 66){
+
+//     console.log("full price")
+// }
+// else{
+
+//     console.log(" senior citizen discount")
+// }
+
+
+// =============Task 3======================
+
+// let largeCountries = ["China","India","USA","Indonesia","Pakistan"]
+// console.log("The 5 largest countries in the world:")
+
+// for(i=0; i < largeCountries.length; i++){
+
+//     console.log( "-"+largeCountries[i] )
+// }
+
+
+// =============Task 4=======================
+
+// let largeCountries = ["Tuvalu","India","USA","Indonesia","Monaco"]
+
+// largeCountries.pop()
+// console.log(largeCountries)
+// largeCountries.push("Pakistan")
+// console.log(largeCountries)
+// largeCountries.shift()
+// console.log(largeCountries)
+// largeCountries.unshift("China")
+// console.log(largeCountries)
+
+// ==============Task 5 ==================
+
+// let dayOfMonth = 13
+// let weekday = "Friday"
+
+// if( dayOfMonth === 13 && weekday === "Friday"){
+
+//     console.log("😱")
+// }
+
+
+// ==============Task 6 =================
+
+let hands = ["rock", "paper", "scissor"]
+
+function gameOn(){
+
+    if ( )
+}
+
+   
