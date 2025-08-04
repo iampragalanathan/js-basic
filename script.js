f283d599a6de7be1ee67ee0630e2964b9a4384dc
PRAGALANATHAN A <pragalanathan.eng@gmail.com>
2025-08-04 14:28:49 +0530

    add

--- script.js
+++ script.js
@@ -123,13 +123,13 @@ let tigerbtn = document.getElementById("tigerbtn")
 let imgplace = document.getElementById("picture")
 
 dogbtn.addEventListener("click", function() {
-   imgplace.innerHTML = `<img src="/task9/dog.jpg" alt="" style="width: 250px;"></img>`
+   imgplace.innerHTML = `<img src="/images/dog.jpg" alt="" style="width: 250px;"></img>`
 })
 
 catbtn.addEventListener("click", function() {
-   imgplace.innerHTML = `<img src="/task9/cat.jpeg" alt="" style="width: 250px;"></img>`
+   imgplace.innerHTML = `<img src="/images/cat.jpeg" alt="" style="width: 250px;"></img>`
 })
 
 tigerbtn.addEventListener("click", function() {
-   imgplace.innerHTML = `<img src="/task9/tiger.jpeg" alt="" style="width: 250px;"></img>`
+   imgplace.innerHTML = `<img src="/images/tiger.jpeg" alt="" style="width: 250px;"></img>`
 })
