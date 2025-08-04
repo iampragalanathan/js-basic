f98ac923c6d48136af0e47e782ac87c89baeae7b
PRAGALANATHAN A <pragalanathan.eng@gmail.com>
2025-08-04 10:23:20 +0530

    add

---
+++ task9/script.js
@@ -0,0 +1,16 @@
+let dogbtn = document.getElementById("dogbtn")
+let catbtn = document.getElementById("catbtn")
+let tigerbtn = document.getElementById("tigerbtn")
+let imgplace = document.getElementById("picture")
+
+dogbtn.addEventListener("click", function() {
+   imgplace.innerHTML = `<img src="/task9/dog.png" alt="" style="width: 250px;"></img>`
+})
+
+catbtn.addEventListener("click", function() {
+   imgplace.innerHTML = `<img src="/task9/cat.jpg" alt="" style="width: 250px;"></img>`
+})
+
+tigerbtn.addEventListener("click", function() {
+   imgplace.innerHTML = `<img src="/task9/tiger.jpeg" alt="" style="width: 250px;"></img>`
+})
