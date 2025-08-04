e6d61dbe54be2f38b4d38d6a1bae3d2da6d1b2ab
PRAGALANATHAN A <pragalanathan.eng@gmail.com>
2025-08-04 14:19:48 +0530

    add

--- script.js
+++ script.js
@@ -81,3 +81,16 @@ function gameOn(){
 gameOn()
 
 
+// =============== Task 7==============
+
+let fighters = ["🐉", "🐥", "🐊", "💩", "🦍", "🐢", "🐩", "🦭", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷", "🐆", "🦕", "🦁"]
+
+let stageEl = document.getElementById("stage")
+let fightButton = document.getElementById("fightButton")
+
+fightButton.addEventListener("click", function() {
+    let fighter1 = fighters[Math.floor(Math.random() * fighters.length)]
+    let fighter2 = fighters[Math.floor(Math.random() * fighters.length)]
+
+    stageEl.textContent = `${fighter1} vs ${fighter2}`
+})
