544b207c0a963d2ee97e8c6ffc4ddf4ab66138b5
PRAGALANATHAN A <pragalanathan.eng@gmail.com>
2025-08-04 10:16:17 +0530

    add let

--- script.js
+++ script.js
@@ -39,7 +39,7 @@
 // let largeCountries = ["China","India","USA","Indonesia","Pakistan"]
 // console.log("The 5 largest countries in the world:")
 
-// for(i=0; i < largeCountries.length; i++){
+// for(let i=0; i < largeCountries.length; i++){
 
 //     console.log( "-"+largeCountries[i] )
 // }
